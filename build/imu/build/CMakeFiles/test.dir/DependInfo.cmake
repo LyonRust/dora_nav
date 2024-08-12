7e5601ae9d56291f1ba05268e096e5b06252babe
SUNNYsyy2005 <s70735526@163.com>
2024-08-12 11:20:06 +0800

    finish rust string

--- build/imu/build/CMakeFiles/test.dir/DependInfo.cmake
+++ build/imu/build/CMakeFiles/test.dir/DependInfo.cmake
@@ -8,15 +8,11 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/xiling/dora_nav/build/imu/main.cc" "CMakeFiles/test.dir/main.cc.o" "gcc" "CMakeFiles/test.dir/main.cc.o.d"
+  "/home/sunny/dora_nav/build/imu/main.cc" "CMakeFiles/test.dir/main.cc.o" "gcc" "CMakeFiles/test.dir/main.cc.o.d"
   )
 
-# Targets to which this target links which contain Fortran sources.
-set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
-  )
-
-# Targets to which this target links which contain Fortran sources.
-set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
--- sink/src/main.rs
+++ sink/src/main.rs
@@ -1,5 +1,17 @@
+use dora_node_api::arrow::array::{AsArray, PrimitiveArray};
+use dora_node_api::arrow::datatypes::UInt8Type;
 use dora_node_api::{self, DoraNode, Event};
-use eyre::{bail, Context};
+use eyre::ContextCompat;
+use std::collections::HashMap;
+use std::time::{Duration, Instant};
+
+use dora_node_api::arrow::array::{ArrayRef};
+use dora_node_api::arrow::buffer::ScalarBuffer;
+use std::str;
+
+static LANGUAGE: &str = "Rust";
+static PLATFORM: &str = "i7-8750@2.20GHz";
+static NAME: &str = "dora-rs daemon Rust";
 
 fn main() -> eyre::Result<()> {
     let (_node, mut events) = DoraNode::init_from_env()?;
@@ -7,29 +19,53 @@ fn main() -> eyre::Result<()> {
     while let Some(event) = events.recv() {
         match event {
             Event::Input {
-                id,
-                metadata: _,
+                id: _,
                 data,
-            } => match id.as_str() {
-                "message" => {
-                
-                    println!("Received data length: {}", data.len());
-                    println!("First few bytes: {:?}", data);
+                metadata: _,
+            } => {      
+                // check if new size bracket
+                let array = data
+                    .as_any()
+                    .downcast_ref::<PrimitiveArray<UInt8Type>>()
+                    .expect("Expected a PrimitiveArray of UInt8Type");
 
-                    let received_string: &str =
-                        TryFrom::try_from(&data).context("expected string message")?;
-                    println!("sink received message: {}", received_string);
-                    if !received_string.starts_with("operator received random value ") {
-                        bail!("unexpected message format (should start with 'operator received random value')")
-                    }
-                    if !received_string.ends_with(" ticks") {
-                        bail!("unexpected message format (should end with   'ticks')")
-                    }       
+                let buffer: &ScalarBuffer<u8> = array.values();
+
+                let bytes: &[u8] = unsafe {
+                    std::slice::from_raw_parts(buffer.as_ptr(), buffer.len())
+                };
+                
+                match str::from_utf8(bytes) {
+                    Ok(s) => println!("received message: {}", s),
+                    Err(e) => println!("failed: {}", e),
                 }
-                other => eprintln!("Ignoring unexpected input `{other}`"),
-            },
-            Event::Stop => {
-                println!("Received manual stop");
+            
+                // let time_u64 = array.get(0).context("could not slice data")?;
+                // let t_send = uhlc::NTP64((*time_u64).into());
+
+                // .to_vec() Data Latency
+                // let _owned_data = array.to_vec();
+
+                // Preallocated data
+                // let _ = root_vec
+                // .get_mut(&data.len())
+                // .unwrap()
+                // .copy_from_slice(array);
+
+                // let t_received = system_time_clock();
+
+                // latencies.push((t_received - t_send).to_duration());
+                // let data_len = data.len() * 8;
+                // if data_len != current_size {
+                //     if n > 0 {
+                //         record_results(start, current_size, n, latencies, latency, date);
+                //     }
+                //     current_size = data_len;
+                //     n = 0;
+                //     start = Instant::now();
+                //     latencies = Vec::new();
+                // }
+                // n += 1;
             }
             Event::InputClosed { id } => {
                 println!("Input `{id}` was closed");
@@ -38,5 +74,7 @@ fn main() -> eyre::Result<()> {
         }
     }
 
+    println!("finished");
     Ok(())
 }
+
